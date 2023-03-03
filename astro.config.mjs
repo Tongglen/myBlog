159503fa25086e7817f09d16c793fd2b31237dce
Tongglen <734021831@qq.com>
2023-03-03 12:35:55 +0800

    fix: add

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,23 @@ import {
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vue from '@astrojs/vue';
+// import UnoCSS from '@unocss/astro'
+import UnoCSS from 'unocss/astro'
+
+import '@unocss/reset/tailwind.css'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 
 	integrations: [mdx(), sitemap(),
-		vue({})
+		vue({}),
+		UnoCSS({
+			/* options */
+		}),
+		// UnoCSS({
+		// 	presets: [],
+
+		// })
 	],
 });
