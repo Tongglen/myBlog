083afb74522418728f7ba17e00415cf328557881
Tongglen <734021831@qq.com>
2023-03-08 11:21:38 +0800

    清除注释

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,16 @@ import {
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vue from '@astrojs/vue';
-// import UnoCSS from '@unocss/astro'
 import UnoCSS from 'unocss/astro'
 
 import '@unocss/reset/tailwind.css'
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 
 	integrations: [mdx(), sitemap(),
 		vue({}),
-		UnoCSS({
-			/* options */
-		}),
-		// UnoCSS({
-		// 	presets: [],
+		UnoCSS({}),
 
-		// })
 	],
 });
