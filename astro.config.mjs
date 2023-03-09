122153b770ce0f960a1f6dc344e41491d2f86deb
Tongglen <734021831@qq.com>
2023-03-09 17:25:33 +0800

    add cute blog style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
-import {
-	defineConfig
-} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vue from '@astrojs/vue';
-import UnoCSS from 'unocss/astro'
+import UnoCSS from 'unocss/astro';
+import preact from '@astrojs/preact';
+import '@unocss/reset/tailwind.css';
+// import mdx from '@astrojs/mdx';
 
-import '@unocss/reset/tailwind.css'
-
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-
-	integrations: [mdx(), sitemap(),
-		vue({}),
-		UnoCSS({}),
-
-	],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), vue({}), UnoCSS({}), preact()]
 });
