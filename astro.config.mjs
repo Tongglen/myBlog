a5446ee7793a66bf8ea4d327002695a84759d872
Tongglen <734021831@qq.com>
2023-07-26 09:51:39 +0800

    stytle

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,37 @@ import vue from '@astrojs/vue';
 import UnoCSS from 'unocss/astro';
 import preact from '@astrojs/preact';
 import '@unocss/reset/tailwind.css';
+import { i18n, defaultLocaleSitemapFilter } from "astro-i18n-aut";
+
+const defaultLocale = "en";
+const locales = {
+  en: "en-US", // the `defaultLocale` value must present in `locales` keys
+  es: "es-ES",
+  fr: "fr-CA",
+};
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    redirects: true,
+  },
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), vue({}), UnoCSS({}), preact()],
+  trailingSlash: "always",
+  integrations: [mdx(), sitemap(), vue({}), UnoCSS({}), preact(),
+  i18n({
+      locales,
+      defaultLocale,
+    }),
+    sitemap({
+      i18n: {
+        locales,
+        defaultLocale,
+      },
+      filter: defaultLocaleSitemapFilter({ defaultLocale }),
+    }),],
+  build: {
+    format: "directory",
+  },
   markdown: {
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
